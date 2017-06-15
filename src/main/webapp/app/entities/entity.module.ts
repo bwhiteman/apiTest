7b5e237cfbfbe5d2f641ed3faf5a5e87dd9746fe
yhippa <richard.yhip@gmail.com>
2017-06-15 10:04:28 -0400

    Added entities and relationships

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { ApiTestHotlistModule } from './hotlist/hotlist.module';
+import { ApiTestHotlistRefCodeModule } from './hotlist-ref-code/hotlist-ref-code.module';
+import { ApiTestPersonModule } from './person/person.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        ApiTestHotlistModule,
+        ApiTestHotlistRefCodeModule,
+        ApiTestPersonModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
